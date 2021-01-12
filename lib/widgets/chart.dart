8ead2740a3e639849bc9d0cc35b72f013525e1b0
LeonScot <dmcawahab@gmail.com>
2021-01-12 17:30:34 +0500

    chart in progress

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:phase_1/models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      return {'day': DateFormat.E(weekDay), 'amount': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
